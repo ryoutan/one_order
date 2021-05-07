60c729b9966861f56e70fa6e910d62c3fe1c36c6
Nat_Lin <not052683@gmail.com>
2021-05-07 14:29:10 +0800

    finish Q1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,15 @@ class WelcomeController < ApplicationController
   def index
     @file = File.read("#{Rails.root}/README.md")
   end
+
+  def no_food_meal
+    @meal = Meal.all
+    @no_food_meal = { no_food_meal: [] }
+    @meal.each do |meal|
+      if meal.foods.empty?
+        @no_food_meal[:no_food_meal] << meal.name
+      end
+    end
+    render json: @no_food_meal
+  end
 end
